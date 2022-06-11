b6adfe34e1d82942bd7bdbe527f5509cd92a0957
Kai Zhou <zhoukaisspu@163.com>
2022-06-11 16:10:59 +0800

    Implement the weatherStation and streetLight

--- lightpole/streetLight.py
+++ lightpole/streetLight.py
@@ -9,8 +9,8 @@ class StreetLighting:
         self._brightness = 0
 
     @property
-    def power(self):
-        command = bytes([])
+    def iccard(self):
+        command = bytes([0x04, 0x03, 0x00, 0x00, 0x00, 0x02, 0xC4, 0x5E])
         result = self._channel.queryValue(command=command, responseLength=7)  
         if result is None:
             self._PowserStatus = 'error'
@@ -19,8 +19,49 @@ class StreetLighting:
         return result
 
     @property
+    def inputvoltage(self):
+        command = bytes([0x05, 0x04, 0x00, 0x04, 0x00, 0x01, 0x71, 0x8F])
+        result = self._channel.queryValue(command=command, responseLength=7)  
+        if result is None:
+            self._PowserStatus = 'error'
+        else:
+            self._PowserStatus = 'normal'
+        return result
+
+    @property
+    def outputcurrent(self):
+        command = bytes([0x05, 0x04, 0x00, 0x05, 0x00, 0x01, 0x20, 0x4F])
+        result = self._channel.queryValue(command=command, responseLength=7)  
+        if result is None:
+            self._PowserStatus = 'error'
+        else:
+            self._PowserStatus = 'normal'
+        return result
+
+    @property
+    def outputpower(self):
+        command = bytes([0x05, 0x04, 0x00, 0x06, 0x00, 0x01, 0xD0, 0x4F])
+        result = self._channel.queryValue(command=command, responseLength=7)  
+        if result is None:
+            self._PowserStatus = 'error'
+        else:
+            self._PowserStatus = 'normal'
+        return result
+
+    @property
+    def Electricityconsumption(self):
+        command = bytes([0x05, 0x04, 0x00, 0x08, 0x00, 0x01, 0xB1, 0x8C])
+        result = self._channel.queryValue(command=command, responseLength=7)  
+        if result is None:
+            self._PowserStatus = 'error'
+        else:
+            self._PowserStatus = 'normal'
+        return result
+   
+
+    @property
     def brightness(self):
-        command = bytes([])
+        command = bytes([0x05, 0x06, 0x00, 0x04, 0x00, 0x32, 0x48, 0x5A])
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._brightness = 'error'
--- lightpole/weatherStation.py
+++ lightpole/weatherStation.py
@@ -3,11 +3,84 @@ class WeatherStationDevice:
         self._channel = channel
         self._status = "normal"
 
-    def getWindSpeed(self):
-        command = bytes([0x01, 0x03, 0x00, 0x09, 0x00, 0x01, 0x54, 0x08])
+    @property
+    def windSpeed(self):
+        command = bytes([0x02, 0x03, 0x00, 0x00, 0x00, 0x01, 0x84, 0x39])
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
-            self._status = "error"
+            self._windSpeedStatus = "error"
+        else:
+            self._windSpeedStatus = "normal"
+        return result
+
+    @property
+    def temperature(self):
+        command = bytes([0x01, 0x03, 0x01, 0xF5, 0x00, 0x01, 0x95, 0xC4])
+        result = self._channel.queryValue(command=command, responseLength=7)
+        if result is None:
+            self._temperatureStatus = "error"
+        else:
+            self._temperatureStatus = "normal"
+        return result
+
+    @property
+    def humidity(self):
+        command = bytes([0x01, 0x03, 0x01, 0xF4, 0x00, 0x01, 0xC4, 0x04])
+        result = self._channel.queryValue(command=command, responseLength=7)
+        if result is None:
+            self._humidityStatus = "error"
+        else:
+            self._humidityStatus = "normal"
+        return result
+
+    @property
+    def illuminance(self):
+        command = bytes([0x01, 0x03, 0x01, 0xFA, 0x00, 0x02, 0xE5, 0xC6])
+        result = self._channel.queryValue(command=command, responseLength=7)
+        if result is None:
+            self._illuminanceStatus = "error"
+        else:
+            self._illuminanceStatus = "normal"
+        return result
+
+    @property
+    def moise(self):
+        command = bytes([0x01, 0x03, 0x01, 0xF6, 0x00, 0x01, 0x65, 0xC4])
+        result = self._channel.queryValue(command=command, responseLength=7)
+        if result is None:
+            self._noiseStatus = "error"
+        else:
+            self._noiseStatus = "normal"
+        return result
+    
+    @property
+    def rain(self):
+        command = bytes([0x03, 0x03, 0x00, 0x00, 0x00, 0x01, 0x85, 0xE8])
+        result = self._channel.queryValue(command=command, responseLength=7)
+        if result is None:
+            self._rainStatus = "error"
+        else:
+            self._rainStatus = "normal"
+        return result
+
+    @property
+    def pm2_5(self):
+        command = bytes([0x01, 0x03, 0x01, 0xF7, 0x00, 0x01, 0x34, 0x04])
+        result = self._channel.queryValue(command=command, responseLength=7)
+        if result is None:
+            self._pm2_5Status = "error"
+        else:
+            self._pm2_5Status = "normal"
+        return result
+
+    @property
+    def pm10(self):
+        command = bytes([0x01, 0x03, 0x01, 0xF8, 0x00, 0x01, 0x04, 0x07])
+        result = self._channel.queryValue(command=command, responseLength=7)
+        if result is None:
+            self._pm10Status = "error"
+        else:
+            self._pm10Status = "normal"
         return result
 
 

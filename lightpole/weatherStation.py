a22881a203638a2d3f6e78f678e1a11a372c577f
Kai Zhou <zhoukaisspu@163.com>
2022-06-12 23:00:42 +0800

    Add test code for weatherStation

--- lightpole/weatherStation.py
+++ lightpole/weatherStation.py
@@ -17,6 +17,7 @@ class WeatherStationDevice:
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._windSpeedStatus = "error"
+            result = 0
         else:
             self._windSpeedStatus = "normal"
             result = int(hex(result[4]<<8|result[5]),16)
@@ -29,6 +30,7 @@ class WeatherStationDevice:
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._temperatureStatus = "error"
+            result = 0
         else:
             self._temperatureStatus = "normal"
             result = int(hex(result[4]<<8|result[5]),16)
@@ -41,6 +43,7 @@ class WeatherStationDevice:
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._humidityStatus = "error"
+            result = 0
         else:
             self._humidityStatus = "normal"
             result = int(hex(result[4]<<8|result[5]),16)
@@ -53,6 +56,7 @@ class WeatherStationDevice:
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._illuminanceStatus = "error"
+            result = 0
         else:
             self._illuminanceStatus = "normal"
             result = int(hex(result[4]<<8|result[5]),16)
@@ -65,6 +69,7 @@ class WeatherStationDevice:
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._noiseStatus = "error"
+            result = 0
         else:
             self._noiseStatus = "normal"
             result = int(hex(result[4]<<8|result[5]),16)
@@ -77,6 +82,7 @@ class WeatherStationDevice:
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._rainStatus = "error"
+            result = 0
         else:
             self._rainStatus = "normal"
             result = int(hex(result[4]<<8|result[5]),16)
@@ -89,6 +95,7 @@ class WeatherStationDevice:
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._pm2_5Status = "error"
+            result = 0
         else:
             self._pm2_5Status = "normal"
             result = int(hex(result[4]<<8|result[5]),16)
@@ -101,6 +108,7 @@ class WeatherStationDevice:
         result = self._channel.queryValue(command=command, responseLength=7)
         if result is None:
             self._pm10Status = "error"
+            result = 0
         else:
             self._pm10Status = "normal"
             result = int(hex(result[4]<<8|result[5]),16)
@@ -133,11 +141,31 @@ class WeatherStationDevice:
 
 if __name__ == "__main__":
     from .uartChannel import UartChannel
+    import time
     uartChannel = UartChannel()
     dev = WeatherStationDevice(channel=uartChannel)
-    speed = dev.getWindSpeed()
-    print(len(speed))
-    print(speed)
-    
-
+    while True:
+        print(">>>>>>>>开始数据查询>>>>>>>")
+        print("windSpeed:", dev.windSpeed())
+        time.sleep(5)
+        print("temperature:", dev.temperature())
+        time.sleep(5)
+        print("humidity:", dev.humidity())
+        time.sleep(5)
+        print("illuminance:", dev.illuminance())
+        time.sleep(5)
+        print("noise:", dev.noise())
+        time.sleep(5)
+        print("rain:", dev.rain())
+        time.sleep(5)
+        print("pm2_5:", dev.pm2_5())
+        time.sleep(5)
+        print("pm10:", dev.pm10())
+        time.sleep(5)
+        print("carbonDioxide:", dev.carbonDioxide())
+        print("<<<<<<<<<查询结束<<<<<<<<<<<<<")
+        print("")
+        print("")
+        print("")
+        time.sleep(30)
     
